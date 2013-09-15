ae01ba23eee1fcf6583914b023507f41a089547e
Roshan Mathews <rmathews@gmail.com>
2013-09-15 11:06:38 +0530

    Section 1.3.3

--- ch1.scm
+++ ch1.scm
@@ -672,3 +672,129 @@
 
 ;; (sum-of-squares-of-primes-between 2 10)
 ;; (product-of-relatively-prime-to-n 10)
+
+;; half interval method
+(define (search f neg-point pos-point)
+  (let ([midpoint (average neg-point pos-point)])
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ([test-value (f midpoint)])
+          (cond [(positive? test-value)
+                 (search f neg-point midpoint)]
+                [(negative? test-value)
+                 (search f midpoint pos-point)]
+                [else midpoint])))))
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ([a-value (f a)]
+        [b-value (f b)])
+    (cond [(and (negative? a-value) (positive? b-value))
+           (search f a b)]
+          [(and (negative? b-value) (positive? a-value))
+           (search f b a)]
+          [else (error "Values are not of oppsosite signs" a b)])))
+
+;; (half-interval-method sin 2.0 4.0)
+;; (half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+;;                       1.0
+;;                       2.0)
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ([next (f guess)])
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  ;; (trace try)
+  (try first-guess))
+
+;; (fixed-point cos 1.0)
+;; (fixed-point (lambda (y) (+ (sin y) (cos y)))
+;;              1.0)
+
+(define (sqrt-fp x)
+  (fixed-point (lambda (y) (average y (/ x y)))
+               1.0))
+
+;; (sqrt-fp 5)
+;; (sqrt-fp 9)
+;; (sqrt-fp 10)
+
+;; Exercise 1.35
+;; (fixed-point (lambda (x) (+ 1 (/ 1.0 x)))
+;;              1.0)
+
+;; Exercise 1.36
+;; instead of display/newline uncomment the (trace try) line in
+;; fixed-point
+;; (fixed-point (lambda (x) (/ (log 1000) (log x))) ;34 iterations
+;;              2.0)
+;; (fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) ;9 iterations
+;;              2.0)
+
+;; Exercise 1.37
+(define (cont-frac-recur n d k)
+  (define (helper i)
+    (if (> i k)
+        0
+        (/ (n i) (+ (d i) (helper (+ i 1))))))
+  ;; (trace helper)
+  (helper 1))
+
+(define (cont-frac-iter n d k)
+  (define (helper i result)
+    (if (= i 0)
+        result
+        (helper (- i 1) (/ (n i) (+ (d i) result)))))
+  ;; (trace helper)
+  (helper k 0))
+
+(define cont-frac cont-frac-recur)
+
+;; (cont-frac (lambda (i) 1.0)
+;;            (lambda (i) 1.0)
+;;            1)
+;; (cont-frac (lambda (i) 1.0)
+;;            (lambda (i) 1.0)
+;;            10)
+;; (cont-frac (lambda (i) 1.0)
+;;            (lambda (i) 1.0)
+;;            20)
+;; (cont-frac (lambda (i) 1.0)
+;;            (lambda (i) 1.0)
+;;            30)
+;; (cont-frac (lambda (i) 1.0)
+;;            (lambda (i) 1.0)
+;;            40)
+
+;; Exercise 1.38
+(define (euler-num i) 1.0)
+(define (euler-den i)
+  (if (= (remainder (+ i 1) 3) 0)
+      (* 2 (/ (+ i 1) 3))
+      1))
+
+;; (+ 2 (cont-frac euler-num euler-den  1))
+;; (+ 2 (cont-frac euler-num euler-den 10))
+;; (+ 2 (cont-frac euler-num euler-den 30))
+;; (+ 2 (cont-frac euler-num euler-den 50))
+
+;; Exercise 1.39
+(define (tan-cf x k)
+  (define (num i)
+    (if (= i 1)
+        x
+        (- (square x))))
+  (define (den i)
+    (- (* i 2) 1))
+  (cont-frac num den k))
+
+;; (tan-cf 0 10)
+;; (tan-cf (/ pi 4) 10)
+;; (tan-cf (/ (* 3 pi) 4) 10)
+;; (tan-cf pi 10)
