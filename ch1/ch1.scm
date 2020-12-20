c9817ba481fcbe8e1292166ae55a153a49f9673d
Roshan Mathews <rmathews@gmail.com>
2020-12-20 20:13:51 +0530

    Exercise 1.10

--- ch1/ch1.scm
+++ ch1/ch1.scm
@@ -170,6 +170,25 @@
 ;; +-v2 is linear iterative
 ;; - 1.9 ---------------------------------------------------
 
+
+;; - Exercise 1.10 -----------------------------------------
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+(A 1 10)    ; 1024
+(A 2 4)     ; 65536
+(A 3 3)     ; 65536
+
+(define (f n) (A 0 n))    ; doubles n
+(define (g n) (A 1 n))    ; 2 power n
+(define (h n) (A 2 n))    ; 2 power (2 power ...)) n times
+;; - 1.10 --------------------------------------------------
+
+
 (define (fib-recursive n)
   ;; return the n-th fibonacci number
   (if (< n 2)
